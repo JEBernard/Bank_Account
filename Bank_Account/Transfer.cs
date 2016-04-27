cfb86639c875f13da022dee022506289027c4be4
Jason Bernard <bernard.jason.e@gmail.com>
2016-04-27 10:58:28 -0500

    Update Transfer.cs

--- Bank_Account/Transfer.cs
+++ Bank_Account/Transfer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -26,23 +26,18 @@ namespace Bank_Account
             double checkingTransferAmount;
             string savingsTransfer = txtSavings.Text;
             double savingsTransferAmount; 
-            double currentCheckingamount; 
-            double currentSavingsamount;
-            // get current amounts
-            currentCheckingamount = double.Parse(accountAdapter.GetTotal("Checking", frmLogin.Username).ToString()); 
-            currentSavingsamount = double.Parse(accountAdapter.GetTotal("Savings", frmLogin.Username).ToString());
-            
+
             var selectedFrom = fromComboBox.Text; 
             var selectedTo = toComboBox.Text; 
             
             // transfer from checking to savings
             if(double.TryParse(checkingTransfer, out checkingTransferAmount) &&  selectedFrom == "Checking" && selectedTo == "Savings") 
             {
-                if(checkingamount > checkingtransfer)
+                if(frmAccount.CheckingBalance > checkingTransferAmount)
                 {
-                    accountAdapter.Deposit(frmLogin.Username, "Savings", checkingamount, frmAccount.datetime);
-                    accountAdapter.Deposit(frmLogin.Username, "Checking", chekcingamount *-1, frmAccount.datetime); 
-                    toolStripStatusLabel1.Text = checkingamount.ToString("C") + " has been transfered from Checking to Savings";
+                    accountAdapter.Deposit(frmLogin.Username, "Savings", checkingTransferAmount, frmAccount.datetime);
+                    accountAdapter.Deposit(frmLogin.Username, "Checking", checkingTransferAmount *-1, frmAccount.datetime); 
+                    toolStripStatusLabel1.Text = checkingTransferAmount.ToString("C") + " has been transfered from Checking to Savings";
                 }
                 else 
                 {
@@ -53,13 +48,14 @@ namespace Bank_Account
             {
                 toolStripStatusLabel1.Text = "Please enter a valid transfer amount";     
             }
+            
             if(double.TryParse(savingsTransfer, out savingsTransferAmount) && selectedFrom == "Savings" && selectedTo == "Checking")
             {
-                if(savingsamount > savingstransfer)
+                if(frmAccount.SavingsBalance > savingsTransferAmount)
                 {
-                    accountAdapter.Deposit(frmLogin.Username, "Checking", savingsamount, frmAccount.datetime); 
-                    accountAdapter.Deposit(frmLogin.Username, "Savings", savingsamount *-1, frmAccount.datetime); 
-                    toolStripStatusLabel1.Text = savingsamount.ToString("C") + " has been transfered from Savings to Checking"; 
+                    accountAdapter.Deposit(frmLogin.Username, "Checking", savingsTransferAmount, frmAccount.datetime); 
+                    accountAdapter.Deposit(frmLogin.Username, "Savings", savingsTransferAmount *-1, frmAccount.datetime); 
+                    toolStripStatusLabel1.Text = savingsTransferAmount.ToString("C") + " has been transfered from Savings to Checking"; 
                 }
                 else 
                 {
